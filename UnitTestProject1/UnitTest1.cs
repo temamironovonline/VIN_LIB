543f9d005e3d27f34ce5925df9ce6bc274f93cd0
Artem Sergeichev <a.sergeichev03@mail.ru>
2023-02-20 13:32:49 +0300

    создание проекта модульного тестирование и подключение к основному проекту

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VIN_LIB;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
